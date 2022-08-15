a95980b0f2493273dc88de2ddddc8dbba61cd857
Pavan <pavanyadav72737@gmail.com>
2022-08-15 05:31:05 -0700

    Commiting the py2

---
+++ 02.py
@@ -0,0 +1,12 @@
+# Number swaping in python
+
+a=float(input("Enter the num"))
+b=float(input("Enter the num"))
+
+print("Before swaping number a={0} and b={1}".format(a,b))
+
+temp=a
+a=b
+b=temp
+
+print("Before swaping number a={0} and b={1}".format(a,b))

7c2779de45d218d4ab808e0f26836b9f605eed1f
Pavan <pavanyadav72737@gmail.com>
2022-08-23 05:47:01 -0700

    Adding the python file

--- Python/06.py
+++ Python/06.py
@@ -1,2 +1,30 @@
 # Oprators in python
 
+a = 19
+b = 6
+
+# Arithmatic Operators
+print("The value of 19 + 6 is", a+b)
+print("The value of 19 - 6 is", a-b)
+print("The value of 19 * 6 is", a*b)
+print("The value of 19 / 6 is", a/b)
+
+# Assignment Operators
+a = 17
+a += 12
+a -= 9
+a *= 2
+a /= 2
+print(a)
+
+# Comparison Operators
+# b=(14>6)
+# b=(14<6)
+# b=(14==6)
+# b=(14>=6)
+# b=(14<=6)
+b=(14!=6)
+print(b)
+
+# Logical Operators
+
